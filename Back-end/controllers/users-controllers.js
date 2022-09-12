d1917048b62f711dcb3c338babfe97709b11e8c3
lenhatminh10042001 <64116754+lenhatminh10042001@users.noreply.github.com>
2022-09-12 12:35:09 +0700

    Finish Backend with MongoDB

--- Back-end/controllers/users-controllers.js
+++ Back-end/controllers/users-controllers.js
@@ -1,55 +1,75 @@
-const uuid = require("uuid").v4;
 const { validationResult } = require("express-validator");
+
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Max Schwarz",
-    email: "test@test.com",
-    password: "test",
-  },
-];
 
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  let users;
+
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError("Fetching users failed, please try again later.", 500);
+    return next(error);
+  }
+  res.json({ users: users.map(user => user.toObject({ getters: true })) });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(new HttpError("Invalid inputs passed, please check your data.", 422));
   }
 
   const { name, email, password } = req.body;
 
-  const hasUser = DUMMY_USERS.find((u) => u.email === email);
-
-  if (hasUser) {
-    throw new HttpError("Could not create user, email already exists.", 422);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email })
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again", 500);
+    return next(error);
   }
 
-  const createdUser = {
-    id: uuid(),
+  if (existingUser) {
+    const error = new HttpError("User exists already, please login instead", 422);
+    return next(error);
+  }
+
+  const createdUser = new User({
     name,
     email,
+    image: 'https://image.shutterstock.com/shutterstock/photos/1672742974/display_1500/stock-photo-eiffel-tower-from-a-height-paris-france-date-of-shooting-year-photo-taken-on-a-drone-1672742974.jpg',
     password,
-  };
+    places: []
+  })
 
-  DUMMY_USERS.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError('Signing up failed, please try again', 500);
+    return next(error);
+  }
 
-  res.status(201).json({ user: createdUser });
+  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError(
-      "Could not identify user, credentials seem to be wrong",
-      401
-    );
+  let existingUser;
+
+  try {
+    existingUser = await User.findOne({ email: email })
+  } catch (err) {
+    const error = new HttpError("Logging in failed, please try again", 500);
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError("Invalid credentials, can not log you in", 401);
+    return next(error);
   }
 
   res.json({ message: "Logged in!" });
